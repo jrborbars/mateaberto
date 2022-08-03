7e61000e22e8af8e79f0a4d8cd5bfa58e2242c94
TheBorba <jrborba.rs@gmail.com>
2022-08-03 18:57:30 -0300

    Working Wifi, RFID card reader, ntp and database.OLED, keyboard and REST not implemented yet.

---
+++ libs/pn532_i2c.py
@@ -0,0 +1,140 @@
+# Adafruit PN532 NFC/RFID control library.
+# Author: Tony DiCola
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2015-2018 Adafruit Industries
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""
+``adafruit_pn532.i2c``
+====================================================
+This module will let you communicate with a PN532 RFID/NFC shield or breakout
+using I2C.
+* Author(s): Original Raspberry Pi code by Tony DiCola, CircuitPython by ladyada,
+             refactor by Carter Nelson
+"""
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_PN532.git"
+
+import time
+from machine import I2C, Pin
+from libs.digitalio import Direction
+from micropython import const
+from libs.adafruit_pn532 import PN532, BusyError, _reset
+
+# pylint: disable=bad-whitespace
+_I2C_ADDRESS = const(0x24)
+
+_NOT_BUSY = const(0x01)
+
+
+class PN532_I2C(PN532):
+    """Driver for the PN532 connected over I2C."""
+
+    def __init__(self, i2c, *, irq=None, reset=None, req=None, debug=False):
+        """Create an instance of the PN532 class using I2C. Note that PN532
+        uses clock stretching. Optional IRQ pin (not used),
+        reset pin and debugging output.
+        """
+        self.debug = debug
+        self._irq = irq
+        self._req = req
+        if reset:
+            # Changed this logic so it is not circuit python dependent (No use of direction)
+            # Do the reset at the I2c level
+            if self.debug:
+                print("reset")
+            reset_pin = Pin(reset, Pin.OUT)
+            reset_pin.value(1)
+            time.sleep(0.1)
+            reset_pin.value(0)
+            time.sleep(0.5)
+            reset_pin.value(1)
+            time.sleep(0.1)
+            # _reset(reset)
+        # self._i2c = I2C.SoftI2C(i2c, _I2C_ADDRESS)
+        self._i2c = i2c
+        # call super.__init__ without reset pin (To get around circuitpython pin specifics)
+        super().__init__(debug=debug)
+
+    def _wakeup(self):  # pylint: disable=no-self-use
+        """Send any special commands/data to wake up PN532"""
+        if self._req:
+            self._req.direction = Direction.OUTPUT
+            self._req.value = True
+            time.sleep(0.1)
+            self._req.value = False
+            time.sleep(0.1)
+            self._req.value = True
+        time.sleep(0.5)
+
+    def _wait_ready(self, timeout=1):
+        """Poll PN532 if status byte is ready, up to `timeout` seconds"""
+        # Updated to use time_ns vs time.monotonic of circuitpython
+        status = bytearray(1)
+        timestamp = time.time_ns()/1000000000
+        if self.debug:
+            print("wait ready timestamp:", timestamp)
+        while ((time.time_ns()/1000000000) - timestamp) < timeout:
+            try:
+                # with self._i2c:
+                self._i2c.readfrom_into(_I2C_ADDRESS, status)
+            except OSError:
+                self._wakeup()
+                continue
+            if status == b'\x01':
+                return True  # No longer busy
+            else:
+                time.sleep(0.05)  # lets ask again soon!
+        # Timed out!
+        if self.debug:
+            print("wait_ready time.time_ns():", time.time_ns()/1000000000)
+        return False
+
+    def _read_data(self, count):
+        """Read a specified count of bytes from the PN532."""
+        if self.debug:
+            print("_read_data")
+        # Build a read request frame.
+        frame = bytearray(count+1)
+        status_byte = bytearray(1)
+        # Updated to use readfrom_into (Circutpython readfrom not supported)
+        self._i2c.readfrom_into(_I2C_ADDRESS, status_byte)
+        if self.debug:
+            print("_read_data status_byte: ", status_byte)
+        if status_byte[0] != _NOT_BUSY:             # not ready
+            if self.debug:
+                print("_read_data busy_error ")
+            raise BusyError
+        if self.debug:
+            print("_read_data readfrom_into")
+        # Updated to use readfrom_into (Circutpython readfrom not supported)
+        self._i2c.readfrom_into(_I2C_ADDRESS, frame)
+        if self.debug:
+            print("_read_data frame: ", frame)
+        return frame[1:]   # don't return the status byte
+
+    def _write_data(self, framebytes):
+        """Write a specified count of bytes to the PN532"""
+        # Updated to using writeto - circuitpython write not supported
+        if self.debug:
+            print('_write data: ', [hex(i) for i in framebytes])
+        self._i2c.writeto(_I2C_ADDRESS, framebytes)
---
+++ rfid_pn532x.py
@@ -0,0 +1,107 @@
+from machine import Pin, SoftI2C
+import time
+import libs.ssd1306 as ssd1306
+# pn_532_i2c wrapper updated by David Somerville to work with Micropython
+import libs.pn532_i2c as pn532_i2c
+import libs.wifimgr as wifimgr
+import ntptime
+import btree
+
+try:
+    f = open("mydb", "r+b")
+except OSError:
+    f = open("mydb", "w+b")
+
+# Now open a database itself
+try:
+    db = btree.open(f)
+    print('DB is open and ok.')
+except OSError as err:
+    print('DB was not opened.', str(err))
+# I2C adresses
+RFID_ID = 36 #0x24
+OLED_ID = 60 #0x3c
+####################
+OLED_W = 128
+OLED_H = 64
+#i2c = I2C( scl=Pin(5), sda=Pin(4), freq=1000000) # GPIO5 and GPIO4 (D1 and D2)
+i2c = SoftI2C( scl=Pin(18), sda=Pin(19), freq=1000000) # GPIO5 and GPIO4 (D1 and D2)
+i2c2 = SoftI2C(scl=Pin(22), sda=Pin(21), freq=1000000)
+
+# wlan is a working network.WLAN(STA_IF) instance.
+wlan = wifimgr.get_connection()
+if wlan is None:
+    print("Could not initialize the network connection.")
+    while True:
+        pass  # you shall not pass :D
+else:
+    print("Network connection OK.")
+time.sleep(5)
+#oled = ssd1306.SSD1306_I2C(OLED_W, OLED_H, i2c2)
+#time.sleep(3)
+
+ntptime.host = 'br.pool.ntp.org'
+ntptime.settime()	# this queries the time from an NTP server
+print('TIME: ',time.localtime())
+
+def scan_i2c(iic):
+    i2c_devices = None
+    try:
+        i2c_devices = iic.scan()
+        print('Scanned')
+    except Exception as e:
+        print('EXP ',str(e))
+    if i2c_devices is not None and len(i2c_devices) > 0:
+        print('Something was found...',i2c_devices)
+        for s in i2c_devices:
+            if s == RFID_ID: 
+                print('PN532 found at I2C address: ', s)
+                break
+            if s == OLED_ID:
+                print('SSD1306 found at I2C address: ', s)
+                break
+    else:
+        print('Nothing was found...')
+    print('Finished')
+            
+pn532 = pn532_i2c.PN532_I2C(i2c, debug=False, reset=5)
+ic, ver, rev, support = pn532.get_firmware_version()
+scan_i2c(i2c)
+scan_i2c(i2c2)
+print(f"Found PN532 with firmware version: {ver}.{rev} | {ic}.{support}")
+#oled.text('Hello, World 1!', 0, 0)
+#oled.text('Hello, World 2!', 0, 10)
+#oled.text('Hello, World 3!', 0, 20)
+        
+#oled.show()
+# Set up configuration for MiFare type cards
+pn532.SAM_configuration()
+print("Waiting RFID/NFC card....")
+last_uid = None
+counter = 0
+while True:
+    uid = pn532.read_passive_target(timeout=0.5)
+    if uid is None:
+        last_uid = None
+        #oled.fill(0)
+        #oled.show()
+        continue
+    if last_uid == uid and uid == MASTER:
+        counter +=1
+    else:
+        print(f"Found card. UID", [hex(i) for i in uid])
+        #oled.fill(0)
+        suid = ""
+        for i in uid:
+            if len(suid) > 0:
+                suid +=":"
+            s = hex(i)[2:4]
+            suid += s
+        #oled.text("UID found",0,0)
+        #oled.text(suid,0,10)
+        #oled.show()
+        print(suid)
+    if counter == 10:
+        counter = 0;
+        
+    last_uid = uid
